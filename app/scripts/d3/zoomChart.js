950a9bf014c7b3531977e31dabaacd536a5da6a3
Winston Fassett <winston.fassett@gmail.com>
2013-02-18 11:59:00 -0600

    got reusable zoom chart working sort of

--- app/scripts/d3/zoomChart.js
+++ app/scripts/d3/zoomChart.js
@@ -7,39 +7,92 @@ function zoomChart(opts) {
       xValue = function(d) { return d[0]; },
       yValue = function(d) { return d[1]; },
       x = opts.x || d3.time.scale(),
-      y = opts.y || d3.scale.linear(),
-      xAxis = d3.svg.axis().scale(x).orient("bottom").tickSize(6, 0),
+      y = opts.y || d3.scale.linear();
+      var tickPadding = 10;
+      var xLabel = "X axis",
+          yLabel = "Y axis";
+      var xAxis = //nv.models.axis().axisLabel(xLabel).showMaxMin(false) // 
+          d3.svg.axis()
+          .scale(x)
+          .orient("bottom")
+          .tickSize(-height)
+          .tickPadding(tickPadding);
+      var yAxis =   //nv.models.axis().axisLabel(yLabel).showMaxMin(false) // 
+        d3.svg.axis()
+          .scale(y)
+          .orient("left")
+          .tickSize(-width)
+          .tickPadding(tickPadding);
+    var
       area = d3.svg.area().x(X).y1(Y),
       line = d3.svg.line().x(X).y(Y);
 
 
-
   function chart(selection, options) {
-
+    // load options
     width = options.width || width;
     height = options.height || height;
 
     console.log('chart', arguments);
     selection.each(function(data) {
-
       // Convert data to standard representation greedily;
       // this is needed for nondeterministic accessors.
       data = data.map(function(d, i) {
         return [xValue.call(data, d, i), yValue.call(data, d, i)];
       });
-
-      // Update the x-scale.
+      // Update scales
       x
-          .domain(d3.extent(data, function(d) { return d[0]; }))
-          .range([0, width - margin.left - margin.right]);
-
-      // Update the y-scale.
+        .domain(d3.extent(data, function(d) { return d[0]; }))
+        .range([0, width - margin.left - margin.right]);
       y
-          .domain([0, d3.max(data, function(d) { return d[1]; })])
-          .range([height - margin.top - margin.bottom, 0]);
-
+        .domain([0, d3.max(data, function(d) { return d[1]; })])
+        .range([height - margin.top - margin.bottom, 0]);
+      
       // Select the svg element, if it exists.
       var svg = d3.select(this).selectAll("svg").data([data]);
+
+      // define svg 
+      var gEnter = svg.enter()
+        .append("svg").append("g")
+          // .append("g")
+	      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+	      .call(d3.behavior.zoom().x(x).y(y).scaleExtent([.2, 5]).on("zoom", draw))
+      // add rect that seems to be required for zoom to work(?)  
+      gEnter.append("rect")
+  	    .attr("class", "background")
+  	    .attr("width", width)
+  	    .attr("height", height);
+      // gEnter.append("path").attr("class", "area");
+
+      // add axes
+      // gEnter.append("g").attr("class", "x axis");
+      gEnter.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
+      gEnter.append("g")
+          .attr("class", "y axis")
+          .call(yAxis);
+
+      var graphEnter = gEnter.append("svg") 
+        .attr("class", "points")
+        .attr("width", width) 
+        .attr("height", height) 
+        .append("g") 
+
+      graphEnter.append("path").attr("class", "line");
+      // graphEnter.append("g").attr("class", "dots")
+      
+      var dots = graphEnter.selectAll("circle")
+        .append("g")
+        .data(function(d){return d})
+            .enter().append("circle")
+            .attr("class", "dot")
+            .attr("r", 8)
+            .attr("transform", translate);
+
+      draw();
+
 
       function draw() {
           console.log('draw', d3.event);
@@ -72,33 +125,19 @@ function zoomChart(opts) {
           //   .attr("r", 8)
           //   .attr("transform", translate);;
           //   // // Update the x-axis.
-            g.select(".x.axis")
-                .attr("transform", "translate(0," + y.range()[0] + ")")
-                .call(xAxis);          
+            g.select(".x.axis").call(xAxis);  
+            g.select(".y.axis").call(yAxis); 
+                // .attr("transform", "translate(0," + y.range()[0] + ")")
+                
+
+          dots.attr("transform", translate);        
       }
-
-      // Otherwise, create the skeletal chart.
-      var gEnter = svg.enter()
-        .append("svg").append("g")
-          // .append("g")
-	      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-	      .call(d3.behavior.zoom().x(x).y(y).scaleExtent([.2, 5]).on("zoom", draw))
-
-      gEnter.append("rect")
-	    .attr("class", "background")
-	    .attr("width", width)
-	    .attr("height", height);
-      // gEnter.append("path").attr("class", "area");
-      gEnter.append("path").attr("class", "line");
-      gEnter.append("g").attr("class", "x axis");
-      gEnter.append("g").attr("class", "dots")
-      draw();
 
     	function translate(d) {
     	  // var rtn = "translate(" + x(d.x) + "," + y(d.y) + ")";
-      	  var rtn = "translate(" + (margin.left + x(d[0])) + "," + (margin.top + y(d[1])) + ")";
-
-    	  return rtn
+      	  var rtn = "translate(" + (x(d[0])) + "," + (y(d[1])) + ")";
+    	  // console.log('translate', rtn);
+        return rtn
     	}
 
     });
